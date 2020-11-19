7ae4793baa659eee899f5664eb1c600135345938
nachomonllor <nachomonllorc@gmail.com>
2020-11-18 21:49:28 -0300

    pedir turno

---
+++ src/app/altas/alta-admin/alta-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-admin',
+  templateUrl: './alta-admin.component.html',
+  styleUrls: ['./alta-admin.component.css']
+})
+export class AltaAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/altas/alta-profesional/alta-profesional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-profesional',
+  templateUrl: './alta-profesional.component.html',
+  styleUrls: ['./alta-profesional.component.css']
+})
+export class AltaProfesionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
